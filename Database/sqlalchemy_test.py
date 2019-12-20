228dbd6ab7cfc786e330542df28f7a45190a6b5d
mremmett <mrr36@njit.edu>
2019-12-19 19:15:00 -0500

    Feature: added all tables and data inserts

--- Database/sqlalchemy_test.py
+++ Database/sqlalchemy_test.py
@@ -24,14 +24,26 @@ class Customer(Base):
     town = Column(String(250), nullable=False)
 
 
-class Address(Base):
-    __tablename__ = 'address'
+class Item(Base):
+    __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    cost_price = Column(Integer, nullable=False)
+    selling_price = Column(Integer, nullable=False)
+    quantity = Column(Integer, nullable=False)
+
+class Order(Base):
+    __tablename__ = 'order'
+    id = Column(Integer, primary_key=True)
     customer_id = Column(Integer, ForeignKey('customer.id'))
-    person = relationship("Customer", backref="addresses")
+    line_items = relationship("OrderLine", backref='order')
+
+class OrderLine(Base):
+    __tablename__ = 'orderline'
+    id = Column(Integer, primary_key=True)
+    order_id = Column(Integer, ForeignKey('order.id'))
+    item_id = Column(Integer, ForeignKey('item.id'))
+    quantity = Column(Integer)
 
 
 # Create all tables in the engine. This is equivalent to "Create Table"
@@ -41,16 +53,10 @@ Base.metadata.create_all(engine)
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-# A DBSession() instance establishes all conversations with the database
-# and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objects in the
-# session won't be persisted into the database until you call
-# session.commit(). If you're not happy about the changes, you can
-# revert all of them back to the last commit by calling
-# session.rollback()
+
 session = DBSession()
 
-# Insert a customer
+
 c1 = Customer(first_name='Toby',
               last_name='Miller',
               username='tmiller',
@@ -75,32 +81,78 @@ c1.id, c2.id
 
 session.commit()
 
-# Insert an Address in the address table using a loop
+c3 = Customer(
+    first_name="John",
+    last_name="Lara",
+    username="johnlara",
+    email="johnlara@mail.com",
+    address="3073 Derek Drive",
+    town="Norfolk"
+)
 
-addresses = [
-   # Address(post_code='00001', person=new_person1),
-   # Address(post_code='00002', person=new_person2),
-   # Address(post_code='00003', person=new_person3),
-]
+c4 = Customer(
+    first_name="Sarah",
+    last_name="Tomlin",
+    username="sarahtomlin",
+    email="sarahtomlin@mail.com",
+    address="3572 Poplar Avenue",
+    town="Norfolk"
+)
 
-# Loop through addresses and commit them to the database
-for address in addresses:
-    session.add(address)
-    session.commit()
+c5 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+              )
 
-# joins Person on Address
-all_people = session.query(Person).join(Address).all()
+c6 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
 
-# Accessing a person with their address, You have to loop the addresses property and remember it was added by the
-# backref on the addresses class
-for person in all_people:
-    # use the __dict__ magic method to have the object print it's properties
-    pprint(person.__dict__)
-    for address in person.addresses:
-        pprint(address.__dict__)
+session.add_all([c3, c4, c5, c6])
+session.commit()
 
-# Retrieving the inverse of the relationship.  Notice I reverse the Person and Address to load the Address table
-all_addresses = session.query(Address).join(Person).all()
-for address in all_addresses:
-    # showing how to use the print function with printing text and data at the same time easily
-    print(f'{address.person.name} has a postal code of {address.post_code}')
+i1 = Item(name='Chair', cost_price=9.21, selling_price=10.81, quantity=5)
+i2 = Item(name='Pen', cost_price=3.45, selling_price=4.51, quantity=3)
+i3 = Item(name='Headphone', cost_price=15.52, selling_price=16.81, quantity=50)
+i4 = Item(name='Travel Bag', cost_price=20.1, selling_price=24.21, quantity=50)
+i5 = Item(name='Keyboard', cost_price=20.1, selling_price=22.11, quantity=50)
+i6 = Item(name='Monitor', cost_price=200.14, selling_price=212.89, quantity=50)
+i7 = Item(name='Watch', cost_price=100.58, selling_price=104.41, quantity=50)
+i8 = Item(name='Water Bottle', cost_price=20.89, selling_price=25, quantity=50)
+
+session.add_all([i1, i2, i3, i4, i5, i6, i7, i8])
+session.commit()
+
+o1 = Order(customer=c1)
+o2 = Order(customer=c1)
+
+line_item1 = OrderLine(order=o1, item=i1, quantity=3)
+line_item2 = OrderLine(order=o1, item=i2, quantity=2)
+line_item3 = OrderLine(order=o2, item=i1, quantity=1)
+line_item3 = OrderLine(order=o2, item=i2, quantity=4)
+
+session.add_all([o1, o2])
+
+session.new
+session.commit()
+
+o3 = Order(customer=c1)
+orderline1 = OrderLine(item=i1, quantity=5)
+orderline2 = OrderLine(item=i2, quantity=10)
+
+o3.order_lines.append(orderline1)
+o3.order_lines.append(orderline2)
+
+session.add_all([o3])
+
+session.commit()
+
+
+
